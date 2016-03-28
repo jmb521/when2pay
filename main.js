152a02afbb52c6bf5cbde24818fc9a83747d0e29
jmb521 <jmp521@gmail.com>
2016-03-27 22:25:23 -0500

    updated js
    fixed both the income and bill forms so that they both didn't repeat
    when the button is clicked.

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var incomearr = [];
 var billarr = [];
 var incomerows, billrows;
 var incomename = document.getElementById("income");
-var frequencydate = document.getSelection("frequency");
+var frequencydate = document.getElementById("frequency");
 var nextpaydate = document.getElementById("nextpay");
 var addIncomeBtn; 
 var bill = document.getElementById("bill");
@@ -19,14 +19,14 @@ var addIncome = function () {
     
     
     incomearr.push({income1: incomename.value, 
-                frequency: frequencydate.options[frequencydate.selectedIndex].text,
+                frequency: frequencydate.value,
                 nextPay: nextpaydate.value});
     for (var i=0; i<incomearr.length; i++) {
       if(incomearr.length >1){
           
           i=incomearr.length-1;
       } 
-     incomerows += "<tr class='incometablerow'><td>" + incomearr[i].income1 + "</td><td>" + incomearr[i].frequency + "</td><td>" + incomearr[i].nextPay + "</td><td><input type='radio' name'input' /></tr>";
+     incomerows += "<tr class='incometablerow'><td>" + incomearr[i].income1 + "</td><td>" + incomearr[i].frequency + "</td><td>" + incomearr[i].nextPay + "</td><td></td></tr>";
     }
    
     $("#incometr").append(incomerows);
@@ -42,14 +42,17 @@ addIncomeBtn.onclick = addIncome;
 
 
 var addBills = function () {
-//    $("#billtr").empty();
+    $(".billtablerow").empty();
     billarr.push({
         bill1: bill.value, 
         billfrequency: billfrequency.value, 
         duedate: duedate.value
     }); 
     for(var j=0; j<billarr.length; j++) {
-        billrows += "<tr><td>" + billarr[j].bill1 + "</td><td>" + billarr[j].billfrequency + "</td><td>" + billarr[j].duedate + "</td></tr>";
+    if(billarr.length >1) {
+        j=billarr.length-1;
+    }
+        billrows += "<tr class='billtablerow'><td>" + billarr[j].bill1 + "</td><td>" + billarr[j].billfrequency + "</td><td>" + billarr[j].duedate + "</td></tr>";
         
     }
     $("#billtr").append(billrows);
